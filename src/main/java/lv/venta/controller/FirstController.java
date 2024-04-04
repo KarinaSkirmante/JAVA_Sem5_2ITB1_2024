af3cf7c66b6f45ad10ca42a2b51fa00567b7b676
KarinaSkirmante <karina.krinkele@gmail.com>
2024-04-04 10:43:27 +0300

    Initial: Seminar 5 for java

---
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -0,0 +1,18 @@
+package lv.venta.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class FirstController {
+	
+	@GetMapping("/hello") //localhost:8080/hello
+	public String getHello() {
+		System.out.println("Hello from Spring boot");
+		return "hello-page"; //tiek parādīta hello-page.html lapa
+	}
+	
+
+}
