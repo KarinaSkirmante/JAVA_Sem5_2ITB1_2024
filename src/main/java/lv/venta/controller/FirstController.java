4682cb4cb889b3ad5eaeb7223d62867cec1886f9
KarinaSkirmante <karina.krinkele@gmail.com>
2024-04-04 11:01:33 +0300

    Add: the second controller

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -1,6 +1,7 @@
 package lv.venta.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -13,6 +14,15 @@ public class FirstController {
 		System.out.println("Hello from Spring boot");
 		return "hello-page"; //tiek parādīta hello-page.html lapa
 	}
+	
+	
+	@GetMapping("/hello/msg")//localhost:8080/hello/msg
+	public String getHelloMsg(Model model) {
+		//                  nosaukums, vērtība
+		model.addAttribute("mymsg"   , "Ziņa no backend!! Hei!");
+		return "msg-page"; //tiek parādīta msg-page.html lapa
+	}
+	
 	
 
 }
