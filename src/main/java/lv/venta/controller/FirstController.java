07975e8cac99277df0eb0ea8fe6872a34296278b
KarinaSkirmante <karina.krinkele@gmail.com>
2024-04-04 12:06:10 +0300

    Add: controller for arraylist

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -1,5 +1,8 @@
 package lv.venta.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,8 +36,20 @@ public class FirstController {
 		return "show-product-page"; //tiek parādīta show-product-page.html lapa
 	}
 	
+	//get mapping funkciju, kura pados sarakstu no vismaz 3 produktiem
+	//html kods, kas so sarakstu avr parādīt
 	
-	
+	@GetMapping("/product/all")//localhost:8080/product/all
+	public String getProductAll(Model model) {
+		Product myProduct1 = new Product("Abols", "Sarkans", 0.99f, 5);
+		Product myProduct2 = new Product("Zemene", "Salda", 1.23f, 3);
+		Product myProduct3 = new Product("Arbuzs", "Roza", 3.99f, 2);
+		ArrayList<Product> allProducts = 
+				new ArrayList<>(Arrays.asList(myProduct1,myProduct2, myProduct3 ));
+		
+		model.addAttribute("myobjs", allProducts);
+		return "show-product-all-page"; //tiek parādīta show-product-all-page.html lapa
+	}
 	
 
 }
