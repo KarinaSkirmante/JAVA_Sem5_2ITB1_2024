ad52135724b2f9ee23fb1ca23c9e91ffc5a5f076
KarinaSkirmante <karina.krinkele@gmail.com>
2024-04-11 12:03:54 +0300

    Add: insert functionality

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -6,10 +6,13 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
 import lv.venta.model.Product;
 import lv.venta.service.ICRUDProductService;
 import lv.venta.service.IFilterProductService;
@@ -53,26 +56,22 @@ public class FirstController {
 
 	@GetMapping("/product/all") // localhost:8080/product/all
 	public String getProductAll(Model model) {
-		try
-		{
+		try {
 			model.addAttribute("myobjs", crudService.retrieveAll());
 			return "show-product-all-page"; // tiek parādīta show-product-all-page.html lapa
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			model.addAttribute("msg", e.getMessage());
 			return "error-page"; // tiek parādīta error-page.html lapa
 		}
-		}
+	}
 
 	@GetMapping("/productone") // localhost:8080/productone?id=2
 	public String getProductoneId(@RequestParam("id") int id, Model model) {
 
-		try
-		{
+		try {
 			model.addAttribute("myobj", crudService.retrieveById(id));
 			return "show-product-page";// tiek parādīta show-product-pahe.html lapa
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			model.addAttribute("msg", e.getMessage());
 			return "error-page"; // tiek parādīta error-page.html lapa
 		}
@@ -82,23 +81,46 @@ public class FirstController {
 	@GetMapping("/product/all/{id}") // localhost:8080/product/all/2
 	public String getProductAllId(@PathVariable("id") int id, Model model) {
 
-		try
-		{
+		try {
 			model.addAttribute("myobj", crudService.retrieveById(id));
 			return "show-product-page";// tiek parādīta show-product-pahe.html lapa
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			model.addAttribute("msg", e.getMessage());
 			return "error-page"; // tiek parādīta error-page.html lapa
 		}
 
 	}
-	
-	
-	//TODO
-	//1. izveido getMapping funkciju, kas atvērs html lapu
-	//2. izveidojam html lapu, kura būs 4 ievades lauki un viena submit poga
-	//3. izveido postmapping funkciju, kura nolasa Product, aks nāk no html 
-	//un ar servisa palīdzību saglabā
+
+	@GetMapping("/product/insert") // localhost:8080/product/insert
+	public String getProductInsert(Model model) {
+		model.addAttribute("product", new Product());// padodam tuksu produktu, kuru aizpildīs html pusē
+		return "insert-product-page";// tiek parādīta insert-product-page.html lapa, kurā varēs ievadīt datus
+	}
+
+	@PostMapping("/product/insert")
+	public String postProductInsert(@Valid Product product, BindingResult result) {// iegūst aizpildītu produktu
+
+		if (result.hasErrors()) {
+			return "insert-product-page";
+		} else {
+
+			try {
+				crudService.create(product);
+				return "redirect:/product/all";
+			} catch (Exception e) {
+				return "redirect:/error";// pārlec uz citu endpointu
+			}
+		}
+
+	}
+
+	@GetMapping("/error") // localhost:8080/error
+	public String getError() {
+		return "error-page";
+	}
+
+	// 2. izveidojam html lapu, kura būs 4 ievades lauki un viena submit poga
+	// 3. izveido postmapping funkciju, kura nolasa Product, aks nāk no html
+	// un ar servisa palīdzību saglabā
 
 }
