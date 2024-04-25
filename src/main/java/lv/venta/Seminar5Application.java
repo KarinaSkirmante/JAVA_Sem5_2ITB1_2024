893cc8b075dbf1025eb9841d3a4aded2748ef17c
localuser <localuser@VEA.LV>
2024-04-25 12:06:20 +0300

    Add: test for product repo and database

--- src/main/java/lv/venta/Seminar5Application.java
+++ src/main/java/lv/venta/Seminar5Application.java
@@ -1,13 +1,37 @@
 package lv.venta;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lv.venta.model.Product;
+import lv.venta.repo.IProductRepo;
 
 @SpringBootApplication
 public class Seminar5Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Seminar5Application.class, args);
+	}
+	
+	@Bean //izsauksies sī funkcija automātiski līdz ko sistema būs palaista
+	public CommandLineRunner testDatabaseLayer(IProductRepo productRepo) {
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+			
+				Product p1 = new Product("Abols", "Sarkans", 0.99f, 5);
+				Product p2 =new Product("Zemene", "Salda", 1.23f, 3);
+				Product p3 =new Product("Arbuzs", "Roza", 3.99f, 2);
+				productRepo.save(p1);
+				productRepo.save(p2);
+				productRepo.save(p3);
+				
+				
+			}
+		};
 	}
 
 }
