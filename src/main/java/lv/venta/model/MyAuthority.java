1f142207285c1036dc62c7dd4cd72e6ab700d416
KarinaSkirmante <karina.krinkele@gmail.com>
2024-05-16 10:25:56 +0300

    Update: added linkage between MyUser and MyAuthority

--- src/main/java/lv/venta/model/MyAuthority.java
+++ src/main/java/lv/venta/model/MyAuthority.java
@@ -1,10 +1,17 @@
 package lv.venta.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -31,6 +38,29 @@ public class MyAuthority {
 	@Column(name = "Title")
 	private String title;
 	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "UserAuthorityTable",
+	joinColumns = @JoinColumn(name="Ida"),
+	inverseJoinColumns = @JoinColumn(name = "Idu"))
+	@ToString.Exclude
+	private Collection<MyUser> users = new ArrayList<MyUser>();
 	
+	
+	public MyAuthority(String title)
+	{
+		setTitle(title);
+	}
+	
+	
+	
+	public void addUser(MyUser user) {
+		if(!users.contains(user))
+			users.add(user);
+	}
+	
+	public void removeUser(MyUser user) {
+		if(users.contains(user))
+			users.remove(user);
+	}
 	
 }
--- src/main/java/lv/venta/model/MyUser.java
+++ src/main/java/lv/venta/model/MyUser.java
@@ -1,10 +1,15 @@
 package lv.venta.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -36,9 +41,31 @@ public class MyUser {
 	@Column(name = "Password")
 	private String password;
 	
+	@ManyToMany(mappedBy = "users", fetch = FetchType.EAGER)
+	private Collection<MyAuthority> authorities = new ArrayList<MyAuthority>();
 	
-	public MyUser (String username, String password) {
+	
+	public MyUser (String username, String password, MyAuthority ... auths) {
 		setUsername(username);
+		setPassword(password);
+		for(MyAuthority tempA: auths)
+			addAuthority(tempA);
 	}
+	
+	
+	public void addAuthority(MyAuthority authority) {
+		if(!authorities.contains(authority))
+			authorities.add(authority);
+	}
+	
+	public void removeAuthority(MyAuthority authority) {
+		if(authorities.contains(authority))
+			authorities.remove(authority);
+	}
+	
+	
+	
+	
+	
 	
 }
