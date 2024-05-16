a06ac3147c5f9b379254c3dd3dd570300b59ea1c
Lara Bernardes <contatolarabernardes@gmail.com>
2024-05-16 10:46:45 +0300

    small fix

--- src/main/java/lv/venta/repo/IMyUserRepo.java
+++ src/main/java/lv/venta/repo/IMyUserRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import lv.venta.model.MyUser;
 
-public interface MyUserRepo extends CrudRepository<MyUser, Integer>{
+public interface IMyUserRepo extends CrudRepository<MyUser, Integer>{
 
 }
